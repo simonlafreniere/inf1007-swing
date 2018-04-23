1efddd71850798306ada7bd4d1f0ce35f37f2aec
Simon <simon.lafreniere@uqtr.ca>
2018-04-23 11:28:04 -0400

    rand

--- inf1007/src/inf1007/PaiementAutentification.java
+++ inf1007/src/inf1007/PaiementAutentification.java
@@ -1,9 +1,21 @@
 package inf1007;
 
+import java.util.Date;
+import java.util.Random;
+
 public class PaiementAutentification {
 
 	
-	public static void autentification(CarteCredit carte){
+	public static int autentification(CarteCredit carte){
 		
+		Date today = new Date();
+		if(carte.getDateExpiration().after(today))
+			return 1;
+		
+		Random rnd=new Random();
+		int num=rnd.nextInt(10);
+		
+		
+		return 0;
 	}
 }
